aad1d6326ff8e452d93a8e90cdd6b8a7464e61f1
PAINFUEGO <kystax69@gmil.com>
2023-11-01 11:39:09 +0530

    example

--- events/custom/dokdo.js
+++ events/custom/dokdo.js
@@ -1,8 +1,17 @@
 /** @format */
 
+const { RateLimitManager } = require('@sapphire/ratelimits');
+const rateLimitManager = new RateLimitManager(10000, 2);
 module.exports = {
   name: 'dokdo',
   run: async (client, message, prefix) => {
+    const rl = rateLimitManager.acquire(`${message.author.id}`);
+
+    if (rl.limited) {
+      return message.reply('Dokdo rate limited');
+    }
+
+    rl.consume();
     new (require('dokdo'))(client, {
       owners: client.owners,
       aliases: ['jsk', 'Jsk'],
