ed5a71b78561420268241aa3b0ebb27c872cc29b
PAINFUEGO <kystax69@gmil.com>
2023-11-05 14:02:13 +0530

    idk what to write but yeah some minor stuff changes

--- plugins/logger.js
+++ plugins/logger.js
@@ -4,57 +4,54 @@ const chalk = require("chalk");
 const moment = require("moment");
 
 module.exports = class logger {
-  static log(content, type = "log", client = "Client") {
+  static log(content, type = "log", client = "Process") {
     const date = moment().format("DD-MM-YYYY hh:mm:ss");
 
     const logTypeColors = {
       log: {
-        color: (text) => chalk.hex("#ffffff")(text),
+        headingColor: (text) => chalk.hex("#ffffff")(text),
+        loggingColor: (text) => chalk.hex("#ffffff")(text),
         display: "Log",
       },
       warn: {
-        color: (text) => chalk.hex("#ffaa00")(text),
+        headingColor: (text) => chalk.hex("#ffaa00")(text),
+        loggingColor: (text) => chalk.hex("#880088")(text),
         display: "Warning",
       },
       error: {
-        color: (text) => chalk.hex("#ff2200")(text),
-        display: "Err",
+        headingColor: (text) => chalk.hex("#ff2200")(text),
+        loggingColor: (text) => chalk.hex("#880088")(text),
+        display: "Error",
       },
       debug: {
-        color: (text) => chalk.hex("#eeee55")(text),
+        headingColor: (text) => chalk.hex("#dddd55")(text),
+        loggingColor: (text) => chalk.hex("#880088")(text),
         display: "Debug",
       },
       cmd: {
-        color: (text) => chalk.hex("#77ff22")(text),
+        headingColor: (text) => chalk.hex("#ff2277")(text),
+        loggingColor: (text) => chalk.hex("#880088")(text),
         display: "Command",
       },
       event: {
-        color: (text) => chalk.hex("#e1f507")(text),
+        headingColor: (text) => chalk.hex("#0088cc")(text),
+        loggingColor: (text) => chalk.hex("#880088")(text),
         display: "Event",
       },
       ready: {
-        color: (text) => chalk.hex("#ff88cc")(text),
+        headingColor: (text) => chalk.hex("#77ee55")(text),
+        loggingColor: (text) => chalk.hex("#880088")(text),
         display: "Ready",
       },
       database: {
-        color: (text) => chalk.hex("#55ff22")(text),
+        headingColor: (text) => chalk.hex("#55cc22")(text),
+        loggingColor: (text) => chalk.hex("#880088")(text),
         display: "Database",
       },
-      player: {
-        color: (text) => chalk.hex("#22aaff")(text),
-        display: "Player",
-      },
-      lavalink: {
-        color: (text) => chalk.hex("#ff8800")(text),
-        display: "Lavalink",
-      },
       cluster: {
-        color: (text) => chalk.hex("#ff2200")(text),
+        headingColor: (text) => chalk.hex("#00cccc")(text),
+        loggingColor: (text) => chalk.hex("#880088")(text),
         display: "Cluster",
-      },
-      manager: {
-        color: (text) => chalk.hex("#22ff00")(text),
-        display: "Manager",
       },
     };
 
@@ -62,12 +59,13 @@ module.exports = class logger {
 
     const logMessage =
       chalk.bold(
-        `${chalk.hex("#2222FF")(date)} - [${chalk.hex("#222255")(
-          client,
-        )}] ${" ".repeat(15 - client.length)}=>   ${log.color(
-          log.display + " ".repeat(9 - log.display.length),
-        )} - > `,
-      ) + `${chalk.hex("#880088")(content)}`;
+        `${chalk.hex("#2222FF")(date)} -   ` +
+          `${chalk.hex("#222255")(client)} ` +
+          `${" ".repeat(15 - client.length)}=>   ` +
+          `${log.headingColor(
+            log.display + " ".repeat(9 - log.display.length),
+          )} - `,
+      ) + `${log.loggingColor(content)}`;
 
     console.log(logMessage);
   }
--- utils/web_server.js
+++ utils/web_server.js
@@ -1,28 +1,23 @@
 /** @format */
 
-const express = require("express");
-const app = express();
-
+const app = require("express")();
 const port = process.env.PORT || 443;
+const logger = require("@plugins/logger");
 
 app.get("/", (req, res) => {
-  var ip;
-  if (req.headers.hasOwnProperty("x-forwarded-for"))
-    ip = req.headers["x-forwarded-for"];
-  else ip = req.connection.remoteAddress;
   res.send(
     '<meta http-equiv="refresh" content="0; URL=https://home.1st-dev.repl.co"/>',
   );
-  fetch(`https://freeipapi.com/api/json/${ip.split(":")[3]}`)
-    .then(async (res) => {
-      res = await res.json();
-      console.log(res);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
 });
 
+app.get("/support", (req, res) => {
+  res.send(
+    '<meta http-equiv="refresh" content="0; URL=https://discord.gg/uY5BXAfd"/>',
+  );
+});
+
+app.use(require("express-status-monitor")());
+
 app.listen(port, () => {
-  console.log("Web server on port :" + port);
+  logger.log(`Web server ready(${port})`, `ready`);
 });
